2def320f15670d9ff286b5020d00f6ad679f2668
zgfei <m18317713325@163.com>
2018-03-28 17:54:15 +0800

    添加nexus库的引用

--- app/src/main/java/com/example/zhaogaofei/testmavnlibrary/MainActivity.java
+++ app/src/main/java/com/example/zhaogaofei/testmavnlibrary/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.zhaogaofei.testmavnlibrary;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.testlibrary.Test;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Test test = new Test();
+        test.printLibraryName();
+        test.printName();
 
     }
 }
